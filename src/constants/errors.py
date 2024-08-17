0427f3060a4beca985792f17eda8679dfb966c90
Choi Wan Yip <yipcw9@gmail.com>
2024-08-17 18:41:40 +0100

    init db conn, create and insert

--- src/constants/errors.py
+++ src/constants/errors.py
@@ -1,3 +1,3 @@
 ERR_INVALID_INPUT = "[ERROR] invalid input"
-
 ERR_READ_CSV = "[ERROR] failed to read CSV data"
+ERR_DB_CONN = "[ERROR] failed to connect to db"
--- src/modules/view_db.py
+++ src/modules/view_db.py
@@ -1,17 +1,72 @@
 # Handles view of entries and properties for clothing items
+# SETUP: open pgAdmin 4
+import psycopg2
 import pandas as pd
-from constants.errors import ERR_READ_CSV
+from constants.errors import ERR_DB_CONN
 
-CSV_PATH = './src/databases/clothing_inventory.csv'
+# Initial db connection & cursor
+try:
+    conn = psycopg2.connect(database="my_closet",
+                            host="localhost",
+                            user="yipcw",
+                            password="",
+                            port="5432")
+except:
+    print(ERR_DB_CONN)
 
-def refresh_db():
-    # Instantiate dataframe of clothes objects
-    # TODO: perhaps update later to prevent need to instantiate every time
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE * FROM my_closet")
+print("ans: ", cursor.fetchone())
+conn.close()
 
-    df = pd.read_csv(CSV_PATH)
-    return df
+RESET_DB = '''
+DROP TABLE IF EXISTS clothing_type, colours, dates_worn;
+'''
 
-def view_db(df):
-    print(df)
+INIT_TABLES = '''
+CREATE TABLE clothing_entry(
+    clothing_id SERIAL PRIMARY KEY,
+    brand_name VARCHAR(50) NOT NULL,
+    clothing_type VARCHAR(50) NOT NULL,
+    filename TEXT NOT NULL,
+    date_bought DATE,
+    wear_count INTEGER DEFAULT 0,
+    is_exists BOOLEAN DEFAULT TRUE,
+);
 
-    return 0
+CREATE TABLE dates_worn(
+    date_id SERIAL PRIMARY KEY,
+    clothing_id INTEGER REFERENCES clothing_entry(clothing_id) ON DELETE CASCADE,
+    date_worn DATE NOT NULL,
+    season VARCHAR(50)
+);
+
+CREATE TABLE colours(
+    colour_id SERIAL PRIMARY KEY,
+    clothing_id INTEGER REFERENCES clothing_entry(clothing_id) ON DELETE CASCADE,
+    colour VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE description_category_names(
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL UNIQUE
+);
+INSERT INTO description_category_names (category_name) VALUES ('Colour', 'Pattern', 'Other');
+
+CREATE TABLE description_fields(
+    description_id SERIAL PRIMARY KEY,
+    category_id INTEGER REFERENCES description_categories(category_id) ON DELETE CASCADE;
+);
+
+CREATE TABLE clothing_descriptions (
+    clothing_id INTEGER REFERENCES clothing_pieces(clothing_id) ON DELETE CASCADE,
+    value_id INTEGER REFERENCES description_values(value_id) ON DELETE CASCADE,
+    PRIMARY KEY (clothing_id, value_id)
+);
+'''
+
+STARTER_CLOTHES = 'INSERT INTO clothing_entry (brand_name, clothing_type, date_bought) VALUES (%s) RETURNING id'
+# clothing_id = cur.fetchone()[0]
+STARTER_DESCRIPTION = 'INSERT INTO description_fields (clothing_id, category_id) VALUES (%s, %s) RETURNING id'
+STARTER_DATES_WORN = 'INSERT INTO dates_worn (clothing_id, date)'
+# sample execution: cur.execute(STARTER_DESCRIPTION, clothing_id, )
