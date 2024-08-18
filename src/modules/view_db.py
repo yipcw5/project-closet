e7742265cde1314a338a5509e3ddc1307dac5bc0
Choi Wan Yip <yipcw9@gmail.com>
2024-08-18 17:52:45 +0100

    integrate init view_db functionality into main.py

--- src/modules/view_db.py
+++ src/modules/view_db.py
@@ -4,24 +4,25 @@ import psycopg2
 from constants.errors import ERR_DB_CONN
 from constants.db_commands import *
 
-# Initial db connection & cursor
-try:
-    conn = psycopg2.connect(database="my_closet",
-                            host="localhost",
-                            user="choiwanyip",
-                            password="",
-                            port="5432")
-except:
-    print(ERR_DB_CONN)
+def view_db():
+    # Initial db connection & cursor
+    try:
+        conn = psycopg2.connect(database="my_closet",
+                                host="localhost",
+                                user="choiwanyip",
+                                password="",
+                                port="5432")
+    except:
+        print(ERR_DB_CONN)
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
-# Setup db tables from scratch
+    # Setup db tables from scratch
 
-cursor.execute(RESET_DB)
-cursor.execute(INIT_TABLES)
-cursor.execute(INIT_FROM_CSV)
-cursor.execute(VIEW_CLOTHING_ENTRIES)
+    cursor.execute(RESET_DB)
+    cursor.execute(INIT_TABLES)
+    cursor.execute(INIT_FROM_CSV)
+    cursor.execute(VIEW_CLOTHING_ENTRIES)
 
-print("ans: ", cursor.fetchall())
-conn.close()
+    print("ans: ", cursor.fetchall())
+    conn.close()
